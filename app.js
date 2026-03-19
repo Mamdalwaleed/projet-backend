618ff055bbf3fc4a07856320bb216168ea3e9dca
mamdal <waleedmamdal96@gmail.com>
2026-03-19 19:34:15 +0100

    ajout de l'authentification et ses routes et middleweare

--- app.js
+++ app.js
@@ -2,15 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose
-  .connect(
-    "mongodb+srv://waleed:abcd123@cluster3.4bk297d.mongodb.net/test?retryWrites=true&w=majority",
-  )
-  .then(() => console.log("Connexion à MongoDB réussie !"))
-  .catch((error) => console.log("Connexion à MongoDB échouée !", error));
+const userRoutes = require("./routes/user");
 
-app.use(express.json());
-
+// CORS
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -24,34 +18,16 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/stuff", (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: "Objet créé !",
-  });
-});
+mongoose
+  .connect(
+    "mongodb+srv://waleed:abcd123@cluster3.4bk297d.mongodb.net/test?retryWrites=true&w=majority",
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((error) => console.log("Connexion à MongoDB échouée !", error));
 
+app.use(express.json());
+
+app.use("/api/auth", userRoutes);
 // 🔹 Route correcte
-app.get("/api/stuff", (req, res, next) => {
-  const stuff = [
-    {
-      _id: "oeihfzeoi",
-      title: "Mon premier objet",
-      description: "Les infos de mon premier objet",
-      imageUrl: "",
-      price: 4900,
-      userId: "qsomihvqios",
-    },
-    {
-      _id: "oeihfzeomoihi",
-      title: "Mon deuxième objet",
-      description: "Les infos de mon deuxième objet",
-      imageUrl: "",
-      price: 2900,
-      userId: "qsomihvqios",
-    },
-  ];
-  res.status(200).json(stuff);
-});
 
 module.exports = app;
